2459a71bf3b1aced1f75d95b79e7791e0e95b28f
Ibka Anhar Fatcha <ibkaanhar1@gmail.com>
2024-06-19 22:18:38 +0700

    feat: multiple changes
    - create new `helvetica_neue` font family and set it to default into the app
    - force app into dark theme

--- app/src/main/java/com/example/recipehub/MainActivity.java
+++ app/src/main/java/com/example/recipehub/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.recipehub;
 
+import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES;
+
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -11,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
