9228c0b68c8f7452fbfd466eca13818c781b82e2
Ibka Anhar Fatcha <ibkaanhar1@gmail.com>
2024-06-21 15:37:13 +0700

    feat: implement recipe detail ui

--- app/src/main/java/com/example/recipehub/RecipeDetail.java
+++ app/src/main/java/com/example/recipehub/RecipeDetail.java
@@ -1,39 +1,24 @@
 package com.example.recipehub;
 
-import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    LinearLayout recipe1;
+public class RecipeDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recipe_detail);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        recipe1 = findViewById(R.id.recipe1);
-
-        recipe1.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, RecipeDetail.class);
-            startActivity(intent);
-        });
-
     }
 }
