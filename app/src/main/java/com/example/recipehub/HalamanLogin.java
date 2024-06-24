180563de9df61ffbed062a9ef564f073aa568d7a
Ibka Anhar Fatcha <ibkaanhar1@gmail.com>
2024-06-24 21:30:43 +0700

    chore: add comment

--- app/src/main/java/com/example/recipehub/HalamanLogin.java
+++ app/src/main/java/com/example/recipehub/HalamanLogin.java
@@ -21,15 +21,24 @@ import com.example.recipehub.models.User;
 import com.example.recipehub.repository.UserRepository;
 
 public class HalamanLogin extends AppCompatActivity {
-    private UserRepository userRepository;
+    /// Deklarasi variable
+    // UserRepository untuk mengakses database ke table User
+    UserRepository userRepository;
 
+    // EditText untuk input username dan password
     EditText username, password;
+
+    // Button untuk login dan TextView untuk register
+    Button loginButton;
+    TextView registerTextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Membuat aplikasi berjalan dalam dark mode
         AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES);
 
+        // Template
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_halaman_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -38,6 +47,7 @@ public class HalamanLogin extends AppCompatActivity {
             return insets;
         });
 
+        // Cek apakah user sudah login
         if (AppPreferences.isUserLoggedIn(this)) {
             Toast.makeText(this, "Welcome back, " + AppPreferences.getUserName(this), Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(this, MainActivity.class);
@@ -45,21 +55,27 @@ public class HalamanLogin extends AppCompatActivity {
             finish();
         }
 
+        // Inisialisasi variable
         DatabaseHelper dbHelper = new DatabaseHelper(this);
         userRepository = new UserRepository(dbHelper.getWritableDatabase());
-        Button loginButton = findViewById(R.id.loginButton);
-        TextView registerTextButton = findViewById(R.id.registerTextButton);
+        loginButton = findViewById(R.id.loginButton);
+        registerTextButton = findViewById(R.id.registerTextButton);
         username = findViewById(R.id.usernameText);
         password = findViewById(R.id.passwordText);
 
+        // Menambahkan fungsionalitas pada button Login
         loginButton.setOnClickListener(v -> {
+            // Mengambil nilai dari EditText username dan password
             String usernameText = username.getText().toString();
             String passwordText = password.getText().toString();
 
+            // Cek apakah ada error yang tidak terduga
             try {
                 User user = userRepository.getUserByUsername(usernameText);
 
+                // Cek apakah terdapat user dengan username dan password yang sesuai
                 if (user.getPassword().equals(passwordText)) {
+                    // Simpan data user yang login ke aplikasi agar aplikasi tau bahwa user telah login
                     AppPreferences.setId(this, user.getId());
                     AppPreferences.setUserName(this, user.getUsername());
 
@@ -76,6 +92,7 @@ public class HalamanLogin extends AppCompatActivity {
             }
         });
 
+        // Menambahkan fungsionalitas pada text Register
         registerTextButton.setOnClickListener(v -> {
             Intent intent = new Intent(this, Register.class);
             startActivity(intent);
--- app/src/main/java/com/example/recipehub/Register.java
+++ app/src/main/java/com/example/recipehub/Register.java
@@ -21,15 +21,17 @@ import com.example.recipehub.repository.RecipeRepository;
 import com.example.recipehub.repository.UserRepository;
 
 public class Register extends AppCompatActivity {
-    private UserRepository userRepository;
-
+    // Deklarasi Variable
+    UserRepository userRepository;
+    // EditText untuk input username, email, dan password
     EditText username, email, password;
-
+    // Button untuk signup
     Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Template
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -38,6 +40,7 @@ public class Register extends AppCompatActivity {
             return insets;
         });
 
+        // Inisialisasi Variable
         DatabaseHelper dbHelper = new DatabaseHelper(this);
         userRepository = new UserRepository(dbHelper.getWritableDatabase());
         signupButton = findViewById(R.id.signupButton);
@@ -45,17 +48,21 @@ public class Register extends AppCompatActivity {
         email = findViewById(R.id.emailText);
         password = findViewById(R.id.passwordText);
 
+        // Menambahkan fungsionalitas pada button signup
         signupButton.setOnClickListener(v -> {
+            // Mengambil data dari inputan
             String usernameText = username.getText().toString();
             String emailText = email.getText().toString();
             String passwordText = password.getText().toString();
 
+            // Cek apakah ada error yang tidak terduga saat signup
             try {
                 User user = new User(0, usernameText, emailText, passwordText);
 
+                // Menambahkan user ke dalam database
                 if (userRepository.addUser(user) != -1) {
                     User createdUser = userRepository.getUserByUsername(usernameText);
-                    // save user data to shared preferences
+                    // Menyimpan data user ke dalam aplikasi agar aplikasi tau bahwa user telah login
                     AppPreferences.setUserName(this, usernameText);
                     AppPreferences.setId(this, createdUser.getId());
 
